bd5cfcbb865e219a33f86435ab8c639d82b99ce4
HighByte0 <byhardihamza@gmail.com>
2024-06-29 16:41:05 +0200

    just i modify login/install npm/install boost/

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './views/pages/login';// Adjust the import paths as needed
+// import Register from './components/Register';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/login" name="Login Page" element={<Login />} />
+
+      
+
+
+      </Routes>
+    </HashRouter>
   );
 }
 
--- src/views/pages/login.js
+++ src/views/pages/login.js
@@ -1,11 +1,9 @@
 import { Link, useNavigate } from 'react-router-dom';
 
 
-import Message from '../../../message'
+
 
 import React, { useState } from 'react';
-import axiosInstance from '../../../maxios/axiosInstance';
-
 import {
   CButton,
   CCard,
@@ -32,82 +30,113 @@ const Login = () => {
 
   const navigate = useNavigate();
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    setError('');
+  };
+
+  const handleSubmit = (e) => {
+    // e.preventDefault();
+    // axiosInstance.post('/login', formData)
+    //   .then((response) => {
+        
+    //     console.log('Login successful:');
+    //     console.log(response.data.data.token)
+
+    //     setTimeout(() => {
+    //       localStorage.setItem('token', response.data.data.token);
+    //       console.log(response.data.data.token)
+    //       navigate('/dashboard'); 
+    //     }, 2000);
+    //     setsuccess(true)
+        
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error during login:', error);
+    //     setError('Error during login');
+        
+    //   });
+  };
 
   return (
     <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center">
-      <CContainer>
-        <CRow className="justify-content-center">
-          <CCol md={8}>
-            <CCardGroup>
-              <CCard className="p-4">
-                <CCardBody>
-              
-                  <CForm onSubmit={handleSubmit}>
-                    <h1>Login</h1>
-                    <p className="text-body-secondary">Sign In to your account</p>
-                    <CInputGroup className="mb-3">
-                      <CInputGroupText>
-                        <CIcon icon={cilUser} />
-                      </CInputGroupText>
-                      <CFormInput 
-                        name="email"
-                        type="email"
-                        placeholder="Email"
-                        autoComplete="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                      />
-                    </CInputGroup>
-                    <CInputGroup className="mb-4">
-                      <CInputGroupText>
-                        <CIcon icon={cilLockLocked} />
-                      </CInputGroupText>
-                      <CFormInput
-                        name="password"
-                        type="password"
-                        placeholder="Password"
-                        value={formData.password}
-                        onChange={handleChange}
-                      />
-                    </CInputGroup>
-                    <CRow>
-                      <CCol xs={6}>
-                        <CButton color="primary" className="px-4" type="submit">
-                          Login
+        <CContainer>
+          <CRow className="justify-content-center">
+            <CCol md={8}>
+              <CCardGroup>
+                <CCard className="p-4">
+                  <CCardBody>
+                
+                    <CForm onSubmit={handleSubmit}>
+                      <h1>Login</h1>
+                      <p className="text-body-secondary">Sign In to your account</p>
+                      <CInputGroup className="mb-3">
+                        <CInputGroupText>
+                          <CIcon icon={cilUser} />
+                        </CInputGroupText>
+                        <CFormInput 
+                          name="email"
+                          type="email"
+                          placeholder="Email"
+                          autoComplete="email"
+                          value={formData.email}
+                          onChange={handleChange}
+                        />
+                      </CInputGroup>
+                      <CInputGroup className="mb-4">
+                        <CInputGroupText>
+                          <CIcon icon={cilLockLocked} />
+                        </CInputGroupText>
+                        <CFormInput
+                          name="password"
+                          type="password"
+                          placeholder="Password"
+                          value={formData.password}
+                          onChange={handleChange}
+                        />
+                      </CInputGroup>
+                      <CRow>
+                        <CCol xs={6}>
+                          <CButton color="primary" className="px-4" type="submit">
+                            Login
+                          </CButton>
+                        </CCol>
+                        <CCol xs={6} className="text-right">
+                          <CButton color="link" className="px-0">
+                            Forgot password?
+                          </CButton>
+                        </CCol>
+                      </CRow>
+                    </CForm>
+                    {/* {error && <Message nbr={1} />}
+                    {success && <Message nbr={0} />} */}
+                  </CCardBody>
+                </CCard>
+                <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
+                  <CCardBody className="text-center">
+                    <div>
+                      <h2>Sign up</h2>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua.
+                      </p>
+                      <Link to="/register">
+                        <CButton color="primary" className="mt-3" active tabIndex={-1}>
+                          Register Now!
                         </CButton>
-                      </CCol>
-                      <CCol xs={6} className="text-right">
-                        <CButton color="link" className="px-0">
-                          Forgot password?
-                        </CButton>
-                      </CCol>
-                    </CRow>
-                  </CForm>
-                  {error && <Message nbr={1} />}
-                  {success && <Message nbr={0} />}
-                </CCardBody>
-              </CCard>
-              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
-                <CCardBody className="text-center">
-                  <div>
-                    <h2>Sign up</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                      tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-                    <Link to="/register">
-                      <CButton color="primary" className="mt-3" active tabIndex={-1}>
-                        Register Now!
-                      </CButton>
-                    </Link>
-                  </div>
-                </CCardBody>
-              </CCard>
-            </CCardGroup>
-          </CCol>
-        </CRow>
-      </CContainer>
-    </div>
+                      </Link>
+                    </div>
+                  </CCardBody>
+                </CCard>
+              </CCardGroup>
+            </CCol>
+          </CRow>
+        </CContainer>
+      </div>
   );
 };
 
